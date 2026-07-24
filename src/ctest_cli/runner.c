adaab6dbf5e0214f467bfa3e13209871e99a2384
WillEdgington <willedge037@gmail.com>
2026-07-24 17:01:47 +0200

    feat: (test cli) implement runner module to orchestrate runner for test cli component

---
+++ src/ctest_cli/runner.c
@@ -0,0 +1,57 @@
+#include "runner.h"
+#include "discovery.h"
+#include "executor.h"
+#include "reporter.h"
+#include "session.h"
+#include <clib/iter.h>
+#include <clib/vector.h>
+#include <stdlib.h>
+
+int session_exit(SessionMetrics *session) {
+  if (session->total_failures == 0 && session->total_crashes == 0) {
+    return 0;
+  }
+  return 1;
+}
+
+int ledger_init(Vector *ledger) {
+  return vector_init(ledger, CTEST_MAX_FAIL_LINE_LEN);
+}
+
+void ledger_free(Vector *ledger) { vector_free(ledger); }
+
+void test_binaries_free(Vector *test_bin) {
+  vector_free(test_bin);
+  free(test_bin);
+}
+
+int ctest_run_session(const char *dir) {
+  SessionMetrics session = {0};
+  Vector ledger;
+  if (ledger_init(&ledger) == -1)
+    return -1;
+
+  ctest_report_start_banner(dir);
+
+  Vector *test_bins = ctest_discover_tests(dir);
+  if (test_bins == NULL) {
+    ledger_free(&ledger);
+    return -1;
+  }
+
+  Iter it = vector_iter(test_bins);
+  while (it.next(&it) == 0) {
+    const char *bin_path = it.current.value;
+    SuiteMetrics metrics = ctest_execute_suite(bin_path, &ledger);
+    ctest_report_suite_metrics(bin_path, &metrics);
+    ctest_update_session(&session, &metrics);
+  }
+  test_binaries_free(test_bins);
+
+  ctest_report_ledger(&ledger);
+  ledger_free(&ledger);
+
+  ctest_report_summary(&session);
+
+  return session_exit(&session);
+}

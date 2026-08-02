f299fe901c4f05a64466d1c471b17f9741c14cfa
WillEdgington <willedge037@gmail.com>
2026-08-02 19:38:51 +0100

    feat: (test cli) allow for directory arguement and help flag for cli runner

--- src/ctest_cli/ctest_cli.c
+++ src/ctest_cli/ctest_cli.c
@@ -1,8 +1,30 @@
 #include "runner.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#define DEFAULT_DIR "./tests"
+int main(int argc, char *argv[]) {
+  const char *target_dir = "./tests";
 
-int main(void) {
-  int status = ctest_run_session(DEFAULT_DIR);
-  return status < 0 ? 2 : status;
+  if (argc == 2) {
+    if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
+      fprintf(stderr, "Usage: %s [test_directory]\n", argv[0]);
+      return 2;
+    }
+    target_dir = argv[1];
+  } else if (argc > 2) {
+    fprintf(stderr, "ctest: invalid arguments\n");
+    fprintf(stderr, "Usage: %s [test_directory]\n", argv[0]);
+    return 2;
+  }
+
+  int status = ctest_run_session(target_dir);
+
+  if (status == -1) {
+    fprintf(stderr, "ctest: error executing test session target '%s'\n",
+            target_dir);
+    return 2;
+  }
+
+  return (status == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }

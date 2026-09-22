2caa02dd7b2f89b8332d7fb5b691ae8cabf2037c
WillEdgington <willedge037@gmail.com>
2026-09-22 13:26:34 +0100

    test: (libctest) add improved test coverage to validate ctest_report_result output in different states

--- tests/libctest/test_libctest.c
+++ tests/libctest/test_libctest.c
@@ -1,6 +1,7 @@
 #define _GNU_SOURCE
 #include <ctest/ctest.h>
 #include <signal.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -77,4 +78,72 @@ CTEST(SUITE_NAME, test_assert_exit_code_success) {
   ASSERT_EXIT_CODE(func_exit_code_forty, NULL, 40,
                    "ASSERT_EXIT_CODE() should pass if expected exit code (40) "
                    "is the actual exit code in the input func");
+}
+
+CTEST(SUITE_NAME, test_report_result_standalone_mode_pass) {
+  const char *runner_active = getenv("CTEST_RUNNER");
+  bool is_runner = (runner_active && strcmp(runner_active, "1") == 0);
+  if (is_runner)
+    unsetenv("CTEST_RUNNER");
+
+  ctest_capture_stdout_start();
+  ctest_report_result(true, "test.c", 10, "1 == 1", "Val: %d", 53);
+  char buf[512];
+  ctest_capture_stdout_end(buf, sizeof(buf));
+
+  const char *expected_pass =
+      "  " CTEST_COLOR_GREEN "[PASS]" CTEST_COLOR_RESET " Val: 53\n";
+
+  if (is_runner)
+    setenv("CTEST_RUNNER", "1", 1);
+
+  ASSERT_STR_EQ(buf, expected_pass,
+                "Test pass output from ctest_report_result() call in "
+                "standalone mode should match the expected formatted string");
+}
+
+CTEST(SUITE_NAME, test_report_result_runner_mode_pass) {
+  const char *runner_active = getenv("CTEST_RUNNER");
+  bool is_runner = (runner_active && strcmp(runner_active, "1") == 0);
+  if (!is_runner)
+    setenv("CTEST_RUNNER", "1", 1);
+
+  ctest_capture_stdout_start();
+  ctest_report_result(true, "test.c", 10, "1 == 1", "Val: %d", 29);
+  char buf[512];
+  ctest_capture_stdout_end(buf, sizeof(buf));
+
+  const char *expected_pass =
+      "PASS" CTEST_TEST_DELIM "test.c" CTEST_TEST_DELIM "10" CTEST_TEST_DELIM
+      "1 == 1" CTEST_TEST_DELIM "Val: 29\n";
+
+  if (!is_runner)
+    unsetenv("CTEST_RUNNER");
+
+  ASSERT_STR_EQ(buf, expected_pass,
+                "Test pass output from ctest_report_result() call in runner "
+                "mode should match the expecter delimiter-separated record");
+}
+
+CTEST(SUITE_NAME, test_report_result_runner_mode_fail) {
+  const char *runner_active = getenv("CTEST_RUNNER");
+  bool is_runner = (runner_active && strcmp(runner_active, "1") == 0);
+  if (!is_runner)
+    setenv("CTEST_RUNNER", "1", 1);
+
+  ctest_capture_stdout_start();
+  ctest_report_result(false, "fail.c", 13, "1 != 1", "This failed");
+  char buf[512];
+  ctest_capture_stdout_end(buf, sizeof(buf));
+
+  const char *expected_fail =
+      "FAIL" CTEST_TEST_DELIM "fail.c" CTEST_TEST_DELIM "13" CTEST_TEST_DELIM
+      "1 != 1" CTEST_TEST_DELIM "This failed\n";
+
+  if (!is_runner)
+    unsetenv("CTEST_RUNNER");
+
+  ASSERT_STR_EQ(buf, expected_fail,
+                "Test fail output from ctest_report_result() call in runner "
+                "mode should match the expecter delimiter-separated record");
 }
